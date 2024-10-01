d8d751667d19d0c82371e644dd0fca0bfd343bed
Abduallh Elsaeid <ba390690@gmail.com>
2024-10-02 00:42:26 +0300

    remove endpoint

--- lib/helper/constans/end_points.dart
+++
@@ -1,7 +0,0 @@
-class EndPoints {
-  static String apiKey = "680961f7cccdd5841fa6b1e5944a2399";
-  static String readAccessToken =
-      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ODA5NjFmN2NjY2RkNTg0MWZhNmIxZTU5NDRhMjM5OSIsIm5iZiI6MTcyNzEyMTgzOS4zNzU3NDUsInN1YiI6IjY2ZjA4ZTBiYTgyYjAwNTcwMzI2NWY4MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nP_5AaWPQGnTnkowjRMtUhUShGO7eluLAo-vmgEt6gE";
-
- 
-}
